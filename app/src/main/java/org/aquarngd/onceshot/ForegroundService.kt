4025d4a2d4ffdac9c80396053dc5f1d09128f5ed
aquamarine5 <50990424+aquamarine5@users.noreply.github.com>
2023-08-18 22:29:31 +0800

    init

---
+++ app/src/main/java/org/aquarngd/onceshot/ForegroundService.kt
@@ -0,0 +1,84 @@
+package org.aquarngd.onceshot
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class ForegroundService: Service() {
+    companion object{
+        val threadName="org.aquarngd.onceshot.screenshot_observer"
+        val notificationId="org.aqaurngd.onceshot.notification"
+        val channelId="org.aquarngd.onceshot.notification_channel"
+    }
+
+    var screenShotListenManager=ScreenShotListenManager.newInstance(this)
+    var isLive=false
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotification()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        isLive=true
+        startFileObserver()
+        return super.onStartCommand(intent, flags, startId)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        isLive=false
+        stopForeground(STOP_FOREGROUND_DETACH)
+    }
+
+    private fun startFileObserver(){
+        screenShotListenManager.setListener {
+            sendNotification(it)
+        }
+        screenShotListenManager.startListen()
+    }
+
+    private fun sendNotification(path:String){
+        val manager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val channel=NotificationChannel(notificationId,channelId,NotificationManager.IMPORTANCE_HIGH)
+            channel.description="Screenshot event"
+            manager.createNotificationChannel(channel)
+        }
+        val builder=NotificationCompat.Builder(this,notificationId).apply {
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            setContentTitle(path)
+            setContentText(path)
+            setWhen(System.currentTimeMillis())
+        }
+        manager.notify(22,builder.build())
+    }
+    private fun createNotification(){
+        val manager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            val channel=NotificationChannel(notificationId,channelId,NotificationManager.IMPORTANCE_HIGH)
+            channel.description="Foreground"
+            manager.createNotificationChannel(channel)
+        }
+        val builder=NotificationCompat.Builder(this,notificationId).apply {
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            setContentTitle("s")
+            setContentText("t")
+            setWhen(System.currentTimeMillis())
+            setOngoing(true)
+        }
+
+        startForeground(11,builder.build())
+    }
+}
---
+++ app/src/main/java/org/aquarngd/onceshot/MainActivity.kt
@@ -0,0 +1,55 @@
+package org.aquarngd.onceshot
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.aquarngd.onceshot.ui.theme.OnceShotTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Intent(this,ForegroundService::class.java).apply {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+                startForegroundService(this)
+            } else{
+                startService(this)
+            }
+        }
+        setContent {
+            OnceShotTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OnceShotTheme {
+        Greeting("Android")
+    }
+}
